c1108c7dfe2658ad2c4b7f2a79d072a4d3b61753
Nadia Shabbar <nshabbar@ucsc.edu>
2022-03-30 12:17:23 -0700

    Adding Kelvin to part 2.

--- Lab01/Lab1_part2.X/part2.c
+++ Lab01/Lab1_part2.X/part2.c
@@ -23,7 +23,8 @@ int main(void) {
     lower = 0; // lower limit of temperature
     upper = 300; // upper limit
     step = 20; //step size
-    fahr = lower;
+    float kelv = lower;
+ 
 
     //print out table
     printf("%5s %5s\n", "F", "C");
@@ -31,6 +32,14 @@ int main(void) {
         celsius = (5.0 / 9.0)*(fahr - 32.0);
         printf("%7.1f %04.0f\n", (double) fahr, (double) celsius);
         fahr = fahr + step;
+    }
+   
+    printf("\n");
+    printf("%5s %5s\n", "K", "F");
+    while (kelv <= upper) {
+        fahr = ((1.8*(kelv -273)) + 32);
+        printf("%03.3f %5f\n", (double) kelv, (double) fahr);
+        kelv = kelv + step;
     }
     /***************************************************************************************************
      * Your code goes in between this comment and the preceding one with asterisks.
