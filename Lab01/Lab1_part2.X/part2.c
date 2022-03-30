a4a06f73dffd60e8e3888378562d276d9af20194
Nadia Shabbar <nshabbar@ucsc.edu>
2022-03-30 11:49:39 -0700

    Adding progress on part 2.

--- Lab01/Lab1_part2.X/part2.c
+++ Lab01/Lab1_part2.X/part2.c
@@ -10,27 +10,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void)
-{
+int main(void) {
     BOARD_Init();
     /***************************************************************************************************
      * Your code goes in between this comment and the following one with asterisks.
      **************************************************************************************************/
     // Declare Variables
-    float fahr, celcius;
+    float fahr, celsius;
     int lower, upper, step;
-    
+
     // Initialize Variables
-    lower=0;    // lower limit of temperature
-    upper=300;  // upper limit
-    step=20;    //step size
-    fahr=lower;
+    lower = 0; // lower limit of temperature
+    upper = 300; // upper limit
+    step = 20; //step size
+    fahr = lower;
 
     //print out table
-    while(fahr<=upper){
-        celsius=(5.0/9.0)*(fahr-32.0);
-        printf("%f %f\n", (double)fahr,(double)celsius);
-        fahr=fahr+step;
+    while (fahr <= upper) {
+        celsius = (5.0 / 9.0)*(fahr - 32.0);
+        printf("%7.1f %04.0f\n", (double) fahr, (double) celsius);
+        fahr = fahr + step;
     }
     /***************************************************************************************************
      * Your code goes in between this comment and the preceding one with asterisks.
