bce89929941c2f2384203521d5f411712d610b93
Nadia Shabbar <nshabbar@ucsc.edu>
2022-04-05 12:00:27 -0700

    Fixed formatting and added the infinite loop.

--- Lab02/Lab2.X/Calculator.c
+++ Lab02/Lab2.X/Calculator.c
@@ -31,99 +31,79 @@ void CalculatorRun(void) {
     double operand1;
     double operand2;
     char sel;
-    printf("\n\nWelcome to NAME's calculator program! Compiled at %s %s\n", __DATE__, __TIME__);
-    printf("Enter a mathematical operation to perform (*,/,+,-,m,a,c,f,t,q,r): ");
-    scanf(" %s", &sel);
-    if (sel == '*') {
-        printf("Enter the first operand: ");
-        scanf(" %s", &operand1);
-        printf("Enter the second operand: ");
-        scanf(" %s", &operand2);
-        double answer = Multiply(operand1, operand2);
-        printf("Result of %lf * %lf: %lf", operand1, operand2, answer);
+    while (1) {
+        printf("\n\nWelcome to NAME's calculator program! Compiled at %s %s\n", __DATE__, __TIME__);
+        printf("Enter a mathematical operation to perform (*,/,+,-,m,a,c,f,t,q,r): ");
+        scanf(" %s", &sel);
+        if (sel == '*') {
+            printf("Enter the first operand: ");
+            scanf(" %lf", &operand1);
+            printf("Enter the second operand: ");
+            scanf(" %lf", &operand2);
+            double answer = Multiply(operand1, operand2);
+            printf("Result of %lf * %lf: %lf", operand1, operand2, answer);
+        } else if (sel == '/') {
+            printf("Enter the first operand: ");
+            scanf(" %lf", &operand1);
+            printf("Enter the second operand: ");
+            scanf(" %lf", &operand2);
+            double answer = Divide(operand1, operand2);
+            printf("Result of %lf / %lf: %lf", operand1, operand2, answer);
+        } else if (sel == '+') {
+            printf("Enter the first operand: ");
+            scanf(" %lf", &operand1);
+            printf("Enter the second operand: ");
+            scanf(" %lf", &operand2);
+            double answer = Add(operand1, operand2);
+            printf("Result of %lf + %lf: %lf", operand1, operand2, answer);
+        } else if (sel == '-') {
+            printf("Enter the first operand: ");
+            scanf(" %lf", &operand1);
+            printf("Enter the second operand: ");
+            scanf(" %lf", &operand2);
+            double answer = Subtract(operand1, operand2);
+            printf("Result of %lf - %lf: %lf", operand1, operand2, answer);
+        } else if (sel == 'm') {
+            printf("Enter the first operand: ");
+            scanf(" %lf", &operand1);
+            printf("Enter the second operand: ");
+            scanf(" %lf", &operand2);
+            double answer = Average(operand1, operand2);
+            printf("Result of (%lf + %lf)/2: %lf", operand1, operand2, answer);
+        } else if (sel == 'a') {
+            printf("Enter an operand: ");
+            scanf(" %lf", &operand);
+            double answer = AbsoluteValue(operand);
+            printf("Result of |%lf|: %lf", operand, answer);
+        } else if (sel == 'c') {
+            printf("Enter an operand: ");
+            scanf(" %lf", &operand);
+            double answer = FahrenheitToCelsius(operand);
+            printf("Result of (%lf - 32)*0.5556: %lf", operand, answer);
+        } else if (sel == 'f') {
+            printf("Enter an operand: ");
+            scanf(" %lf", &operand);
+            double answer = CelsiusToFahrenheit(operand);
+            printf("Result of (%lf * 1.8) + 32: %lf", operand, answer);
+        } else if (sel == 't') {
+            printf("Enter an operand: ");
+            scanf(" %lf", &operand);
+            double answer = Tangent(operand);
+            printf("Result of Tan(%lf): %lf", operand, answer);
+        } else if (sel == 'q') {
+            return;
+        } else if (sel == 'r') {
+            printf("Enter an operand: ");
+            scanf(" %lf", &operand);
+            double answer = Round(operand);
+            printf("Result of ~%lf: %lf", operand, answer);
+        } else {
+            printf("The input was not valid. Please try another input");
+        }
+        // Your code here
+        BOARD_End();
+        while (1);
     }
-    else if(sel == '/')
-    {
-        printf("Enter the first operand: ");
-        scanf(" %s", &operand1);
-        printf("Enter the second operand: ");
-        scanf(" %s", &operand2);
-        double answer = Divide(operand1, operand2);
-        printf("Result of %lf / %lf: %lf", operand1, operand2, answer);
-    }
-    else if(sel == '+')
-    {
-        printf("Enter the first operand: ");
-        scanf(" %s", &operand1);
-        printf("Enter the second operand: ");
-        scanf(" %s", &operand2);
-        double answer = Add(operand1, operand2);
-        printf("Result of %lf + %lf: %lf", operand1, operand2, answer);
-    }
-    else if(sel == '-')
-    {
-        printf("Enter the first operand: ");
-        scanf(" %s", &operand1);
-        printf("Enter the second operand: ");
-        scanf(" %s", &operand2);
-        double answer = Subtract(operand1, operand2);
-        printf("Result of %lf - %lf: %lf", operand1, operand2, answer);
-    }
-    else if(sel == 'm')
-    {
-        printf("Enter the first operand: ");
-        scanf(" %s", &operand1);
-        printf("Enter the second operand: ");
-        scanf(" %s", &operand2);
-        double answer = Average(operand1, operand2);
-        printf("Result of (%lf + %lf)/2: %lf", operand1, operand2, answer);
-    }
-    else if(sel == 'a')
-    {
-        printf("Enter an operand: ");
-        scanf(" %s", &operand);
-        double answer = AbsoluteValue(operand);
-        printf("Result of |%lf|: %lf", operand, answer);
-    }
-    else if(sel == 'c')
-    {
-        printf("Enter an operand: ");
-        scanf(" %s", &operand);
-        double answer = FahrenheitToCelsius(operand);
-        printf("Result of (%lf - 32)*0.5556: %lf", operand, answer);
-    }
-    else if(sel == 'f')
-    {
-        printf("Enter an operand: ");
-        scanf(" %s", &operand);
-        double answer = CelsiusToFahrenheit(operand);
-        printf("Result of (%lf * 1.8) + 32: %lf", operand, answer);
-    }
-    else if(sel == 't')
-    {
-        printf("Enter an operand: ");
-        scanf(" %s", &operand);
-        double answer = Tangent(operand);
-        printf("Result of Tan(%lf): %lf", operand, answer);
-    }
-    else if(sel == 'q')
-    {
-        return;
-    }
-    else if(sel == 'r')
-    {
-        printf("Enter an operand: ");
-        scanf(" %s", &operand);
-        double answer = Round(operand);
-        printf("Result of ~%lf: %lf", operand, answer);
-    }
-    else
-    {
-        printf("The input was not valid. Please try another input");
-    }
-    // Your code here
-    BOARD_End();
-    while (1);
 }
 
 /********************************************************************************
@@ -182,7 +162,7 @@ double FahrenheitToCelsius(double operand) {
  * Define the Celsius to Fahrenheit function here.
  ********************************************************************************/
 double CelsiusToFahrenheit(double operand) {
-    double result =(operand * 1.8) + 32;
+    double result = (operand * 1.8) + 32;
     return result;
 }
 
