cfc20b8cc63043d0db91ebd1ae3a2fa4cee0e68d
Nadia Shabbar <nshabbar@ucsc.edu>
2022-04-12 18:01:36 -0700

    Redoing work. Adding test cases.

--- Lab03/Lab3.X/MatrixMath.c
+++ Lab03/Lab3.X/MatrixMath.c
@@ -17,8 +17,9 @@ int b;
 void MatrixPrint(float mat[3][3]) {
     for (a = 0; a < DIM; a++) {
         for (b = 0; b < DIM; b++) {
-            printf("%f", mat[a][b]);
+            printf("%.4f", mat[a][b]);
         }
+        printf("\n");
     }
 }
 
@@ -26,12 +27,10 @@ int MatrixEquals(float mat1[3][3], float mat2[3][3]) {
     for (a = 0; a < DIM; a++) {
         for (b = 0; b < DIM; b++) {
             if (abs(mat1[a][b] - mat2[a][b]) < FP_DELTA) {
-                return 1;
-            } else {
                 return 0;
             }
         }
-    }
+    }return 1;
 }
 
 void MatrixAdd(float mat1[3][3], float mat2[3][3], float result[3][3]) {
@@ -41,90 +40,3 @@ void MatrixAdd(float mat1[3][3], float mat2[3][3], float result[3][3]) {
         }
     }
 }
-
-void MatrixMultiply(float mat1[3][3], float mat2[3][3], float result[3][3]) {
-    for (a = 0; a < DIM; a++) {
-        for (b = 0; b < DIM; b++) {
-            result[a][b] = mat1[a][b] * mat2[a][b];
-        }
-    }
-}
-
-void MatrixScalarAdd(float x, float mat[3][3], float result[3][3]) {
-    for (a = 0; a < DIM; a++) {
-        for (b = 0; b < DIM; b++) {
-            result[a][b] = mat[a][b] + x;
-        }
-    }
-}
-
-void MatrixScalarMultiply(float x, float mat[3][3], float result[3][3]) {
-    for (a = 0; a < DIM; a++) {
-        for (b = 0; b < DIM; b++) {
-            result[a][b] = mat[a][b] * x;
-        }
-    }
-}
-
-float MatrixTrace(float mat[3][3]) {
-    for (a = 0; a < DIM; a++) {
-        for (b = 0; b < DIM; b++) {
-            if (a == b) {
-                float trace = mat[a][b] + trace;
-            }
-        }
-    }
-}
-
-void MatrixTranspose(float mat[3][3], float result[3][3]) {
-    for (a = 0; a < DIM; a++) {
-        for (b = 0; b < DIM; b++) {
-            result[a][b] = mat[b][a];
-        }
-    }
-}
-
-void MatrixSubmatrix(int i, int j, float mat[3][3], float result[2][2]) {
-    int sub_row = 0;
-    int sub_column = 0;
-    for (a = 0; a < DIM; a++) {
-        if (a == i) {
-            continue;
-        }
-        for (b = 0; b < DIM; b++) {
-            if (b == j) {
-                continue;
-            }
-            result[sub_row][sub_column] = mat[i][j];
-            sub_column++;
-            if (sub_column == 2) {
-                sub_column = 0;
-                sub_row++;
-            }
-
-        }
-    }
-}
-
-float MatrixDeterminant2x2(float mat[2][2]) {
-    for (a = 0; a < 2; a++) {
-        for (b = 0; b < 2; b++) {
-            float det = mat[1][1] - mat[2][2];
-        }
-    }
-}
-
-float MatrixDeterminant(float mat[3][3]) {
-    float submatrix[2][2];
-    int j;
-    for (a = 0; a < DIM; a++) {
-        MatrixSubmatrix(0, j, mat, submatrix);
-        float result = mat[0][0] * MatrixDeterminant2x2(submatrix) 
-        - mat[0][1] * MatrixDeterminant2x2(submatrix) 
-        + mat[0][2] * MatrixDeterminant2x2(submatrix);
-    }
-}
-
-//void MatrixInverse(float mat[3][3], float result[3][3]){
-    
-//}
--- Lab03/Lab3.X/mml_test.c
+++ Lab03/Lab3.X/mml_test.c
@@ -15,19 +15,104 @@
 
 
 // Module-level variables:
-float zero_matrix[3][3] = {{},{},{}};
+float zero_matrix[3][3] = {
+    {},
+    {},
+    {}
+};
+int ttest = 0;
 
-int main()
-{
+int main() {
     BOARD_Init();
 
     printf("Beginning nshabbar's mml test harness, compiled on %s %s\n", __DATE__, __TIME__);
-    
+
     printf("Demonstrating MatrixPrint():\n");
     MatrixPrint(zero_matrix);
 
     // Add more tests here!
-    
+    {
+        int equal_test = 0;
+        int pass_test = 0;
+        float m1[DIM][DIM] = {
+            {1, 1, 1},
+            {1, 1, 1},
+            {1, 1, 1}
+        };
+        float m2[DIM][DIM] = {
+            {1.1, 1.1, 1.1},
+            {1.1, 1.1, 1.1},
+            {1.1, 1.1, 1.1}
+        };
+        float m3[DIM][DIM] = {
+            {1, 2, 3},
+            {1, 2, 3},
+            {1, 2, 3}
+        };
+
+        float test1 = MatrixEquals(zero_matrix, zero_matrix);
+        float test2 = MatrixEquals(m1, m1);
+        float test3 = MatrixEquals(m2, m2);
+        float test4 = MatrixEquals(m3, m3);
+        float test5 = MatrixEquals(m1, m2);
+        float test6 = MatrixEquals(m2, m3);
+
+        if (test1 == 0) {
+            equal_test += 1;
+            pass_test +=1;
+            ttest += 1;
+        } else {
+            printf("Test 1 failed.");
+            equal_test += 1;
+            ttest += 1;
+        }
+        if (test2 == 0) {
+            equal_test += 1;
+            pass_test +=1;
+            ttest += 1;
+        } else {
+            printf("Test 2 failed.");
+            equal_test += 1;
+            ttest += 1;
+        }
+        if (test3 == 0) {
+            equal_test += 1;
+            pass_test +=1;
+            ttest += 1;
+        } else {
+            printf("Test 3 failed.");
+            equal_test += 1;
+            ttest += 1;
+        }
+        if (test4 == 0) {
+            equal_test += 1;
+            pass_test +=1;
+            ttest += 1;
+        } else {
+            printf("Test 4 failed.");
+            equal_test += 1;
+            ttest += 1;
+        }
+        if (test5 == 0) {
+            equal_test += 1;
+            pass_test +=1;
+            ttest += 1;
+        } else {
+            printf("Test 5 failed.");
+            equal_test += 1;
+            ttest += 1;
+        }
+        if (test6 == 0) {
+            equal_test += 1;
+            pass_test +=1;
+            ttest += 1;
+        } else {
+            printf("Test 6 failed.");
+            equal_test += 1;
+            ttest += 1;
+        }
+        printf("%d tests of %d tests passed.", pass_test, equal_test);
+    }
     BOARD_End();
     while (1);
 }
