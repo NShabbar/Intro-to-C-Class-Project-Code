7cdf63fa8ada87c68b88fe20fe9de284e585c4aa
Nadia Shabbar <nshabbar@ucsc.edu>
2022-04-18 20:28:53 -0700

    Adding progress. I am not confident in my last two functions.

--- Lab04/Lab4.X/stack.c
+++ Lab04/Lab4.X/stack.c
@@ -1,13 +1,13 @@
 #include "BOARD.h"
 #include "stack.h"
 
-void StackInit(struct Stack *stack){
+void StackInit(struct Stack * stack) {
     stack->currentItemIndex = -1;
     stack->initialized = TRUE;
 }
 
-int StackPush(struct Stack *stack, double value){
-    if (stack->initialized == TRUE && !StackIsFull(stack)){
+int StackPush(struct Stack *stack, double value) {
+    if (stack->initialized == TRUE && !StackIsFull(stack)) {
         stack->currentItemIndex += 1;
         stack->stackItems[stack->currentItemIndex] = value;
         return SUCCESS;
@@ -15,8 +15,8 @@ int StackPush(struct Stack *stack, double value){
     return STANDARD_ERROR;
 }
 
-int StackPop(struct Stack *stack, double *value){
-    if (stack->initialized && !StackIsEmpty(stack)){
+int StackPop(struct Stack *stack, double *value) {
+    if (stack->initialized && !StackIsEmpty(stack)) {
         *value = stack->stackItems[stack->currentItemIndex];
         stack->currentItemIndex -= 1;
         return SUCCESS;
@@ -24,24 +24,24 @@ int StackPop(struct Stack *stack, double *value){
     return STANDARD_ERROR;
 }
 
-int StackIsEmpty(const struct Stack *stack){
-    if (stack->initialized && stack->currentItemIndex == -1){
+int StackIsEmpty(const struct Stack * stack) {
+    if (stack->initialized && stack->currentItemIndex == -1) {
         return TRUE;
     }
     return FALSE;
 }
 
-int StackIsFull(const struct Stack *stack){
-    if (stack->initialized && stack->currentItemIndex == STACK_SIZE -1){
+int StackIsFull(const struct Stack * stack) {
+    if (stack->initialized && stack->currentItemIndex == STACK_SIZE - 1) {
         return TRUE;
     }
     return FALSE;
 }
 
-int StackGetSize(const struct Stack *stack){
-    if (!stack->initialized){
+int StackGetSize(const struct Stack * stack) {
+    if (stack->initialized) {
         return SIZE_ERROR;
     }
-    return stack->currentItemIndex +1;
+    return stack->currentItemIndex + 1;
 }
 
--- Lab04/Lab4.X/stack_test.c
+++ Lab04/Lab4.X/stack_test.c
@@ -9,59 +9,53 @@
 // User libraries
 #include "stack.h"
 
-int main()
-{
+int main() {
     BOARD_Init();
 
     printf("\n###### Beginning nshabbar's stack test harness: ####\n\n");
 
     //What follows is starter code.  You will need to modify it!
-    
+
     printf("Does StackInit() set the currentItemIndex appropriately?\n");
     struct Stack stack = {};
     StackInit(&stack);
-    if (stack.currentItemIndex == -1) {
-        printf("passed");
-    } else {
-        printf("failed");
-    }
-    
-    printf("Does StackPush() push to an empty stack appropriately?\n");
-    if (StackPush(%stack, 1) == SUCCESS && stack.initialized == TRUE){
+    if (stack.currentItemIndex == -1 && stack.initialized == TRUE) {
         printf("Test passed.\n");
-    }
-    else{
+    } else {
         printf("Test failed.\n");
     }
-    
+
+    printf("Does StackPush() push to an empty stack appropriately?\n");
+    if (StackPush(&stack, 1) == SUCCESS && stack.initialized == TRUE) {
+        printf("Test passed.\n");
+    } else {
+        printf("Test failed.\n");
+    }
+
     printf("Does StackPop() remove the last element appropriately?\n");
-    int test_count;
+    double test_count;
     if (StackPop(&stack, &test_count) == SUCCESS && test_count == 1) {
         printf("Test passed.\n");
-    }
-    else{
+    } else {
         printf("Test failed.\n");
     }
-    
-    printf("Does StackIsEmpty() work properly?");
-    if (StackIsEmpty(&stack) == SUCCESS){
+
+    printf("Does StackIsEmpty() work properly?\n");
+    if (StackIsEmpty(&stack) == SUCCESS) {
         printf("Test passed.\n");
-    }
-    else{
+    } else {
         printf("Test failed.\n");
     }
-    printf("Does StackIsFull work properly?");
-    if (StackIsFull(&stack) == SUCCESS){
+    printf("Does StackIsFull work properly?\n");
+    if (StackIsFull(&stack) == FALSE) {
         printf("Test passed.\n");
-    }
-    else{
+    } else {
         printf("Test failed.\n");
     }
-    printf("Does StackGetSize() retrieve the value properly?");
-    if (StackGetSize(&stack) == SUCCESS && !stack.initialized == SIZE_ERROR){
+    printf("Does StackGetSize() retrieve the value properly?\n");
+    if (StackGetSize(&stack) == SIZE_ERROR && stack.initialized == SUCCESS) {
         printf("Test passed.\n");
-    }
-    else{
+    } else {
         printf("Test failed.\n");
     }
     BOARD_End();
