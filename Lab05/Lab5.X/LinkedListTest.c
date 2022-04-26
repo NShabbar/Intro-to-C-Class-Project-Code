967c4c27f4d36af3a6ccf2f18c3c3993b83bedd0
Nadia Shabbar <nshabbar@ucsc.edu>
2022-04-26 11:12:07 -0700

    Adding more tests to LinkedTest.

--- Lab05/Lab5.X/LinkedListTest.c
+++ Lab05/Lab5.X/LinkedListTest.c
@@ -15,33 +15,79 @@ int main() {
     char *first = "2B";
     char *second = "CP30";
     char *third = "R2D2"";
-    char *last = "8008";
+    char *fourth = "8B";
     
     printf("Creating a new list.\n");
-    struct ListItem = *a = LinkedListNew(first);
+    struct ListItem *a = LinkedListNew(first);
     if (a->previousItem == NULL && a->data == NULL){
         printf("Success.\n");
     }
     
     printf("Adding new items.\n");
-    struct ListItem = *b = LinkedListCreateAfter(a, second);
-    struct ListItem = *c = LinkedListCreateAfter(b, third);
+    struct ListItem *b = LinkedListCreateAfter(a, second);
+    struct ListItem *c = LinkedListCreateAfter(b, third);
     if (b->nextItem == c && b->previousItem == a){
         printf("Success.\n");
     }
+    else{
+        printf("Failed.\n");
+    }
     
     printf("Testing for list size.\n");
-    struct ListItem = *d = LinkedListCreateAfter(c, last);
+    struct ListItem *d = LinkedListCreateAfter(c, fourth);
     int result = LinkedListSize(d);
     int expected = 4;
     if (result == expected){
         printf("Success. 1/2\n");
+    }
+    else{
+        printf("Failed.\n");
     }
     int result_1 = LinkedListSize(a);
     int expected_1 = 4;
     if (result_1 == expected_1){
         printf("Success. 2/2\n");
     }
+    else{
+        printf("Failed.\n");
+    }
+    
+    printf("Testing for first item.\n");
+    struct ListItem  *start = LinkedListGetFirst(a);
+    if (start == a){
+        printf("Success. 1/2\n");
+    }
+    else{
+        printf("Failed.\n");
+    }
+    struct ListItem *two = LinkedListGetFirst(b);
+    if (two == a){
+        printf("Success. 2/2\n");
+    }
+    else{
+        printf("Failed.\n");
+    }
+    
+    printf("Testing for last item.\n");
+    struct ListItem *end = LinkedListGetLast(c);
+    if (end == c){
+        printf("Success. 1/2\n");
+    }
+    else{
+        printf("Failed.\n");
+    }
+    struct ListItem *end_2 = LinkedListGetLast(a);
+    if (end_2 == c){
+        printf("Success. 1/2\n");
+    }
+    else{
+        printf("Failed.\n");
+    }
+    
+    printf("Testing for printed list.");
+    struct ListItem *print_0 = LinkedListPrint(a);
+    printf("Expected: [ %s, %s, %s, %s]\n", first, second, third, fourth);
+    printf("Result: \n");
     
     
 }
