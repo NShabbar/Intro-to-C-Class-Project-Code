bd2154c10d8af2346a3a4d0e5643beb832f974c2
Nadia Shabbar <nshabbar@ucsc.edu>
2022-04-26 11:55:41 -0700

    Finished LinkedListTest.

--- Lab05/Lab5.X/LinkedList.c
+++ Lab05/Lab5.X/LinkedList.c
@@ -5,7 +5,7 @@
 #include "LinkedList.h"
 #include "BOARD.h"
 
-ListItem *LinkedListNew(char *data){
+ListItem *LinkedListNew(char *data) {
     ListItem *point;
     point = malloc(sizeof (ListItem));
     if (point == NULL) {
@@ -17,85 +17,82 @@ ListItem *LinkedListNew(char *data){
     return point;
 }
 
-ListItem *LinkedListCreateAfter(ListItem *item, char *data){
-    ListItem *newlist;
-    newlist = LinkedListNew(data);
-    if (newlist == NULL){
+ListItem *LinkedListCreateAfter(ListItem *item, char *data) {
+    struct ListItem *added_item = (struct ListItem*) malloc(sizeof (ListItem));
+    if (added_item == NULL) {
         return NULL;
     }
-    if (item != NULL){
-        if (item->nextItem != NULL){
-            item->nextItem->previousItem = newlist;
-            newlist->nextItem = item->nextItem;
-            item->nextItem = newlist;
-            newlist->previousItem = item;
-        }
-    }
-    return newlist;
+    added_item ->data = data;
+    added_item ->previousItem = item;
+    added_item ->nextItem = item->nextItem;
+    item->nextItem = added_item;
+    return added_item;
 }
 
-char *LinkedListRemove(ListItem *item){
+char *LinkedListRemove(ListItem *item) {
     char *data = item->data;
-    if (item == NULL){
+    if (item == NULL) {
         return NULL;
     }
-    if (item->nextItem != NULL){
+    if (item->nextItem != NULL) {
         item->nextItem->previousItem = item->previousItem;
-        if (item->previousItem != NULL){
-            item->previousItem->nextItem = item->nextItem;
-            free(item);
-        }
-    }return data;
+    }
+    if (item->previousItem != NULL) {
+        item->previousItem->nextItem = item->nextItem;
+        free(item);
+    }
+
+    return data;
 }
 
-int LinkedListSize(ListItem *list){
-    int count = 1;
+int LinkedListSize(ListItem *list) {
+    int count = 0;
     ListItem *head = LinkedListGetFirst(list);
-    while (head != NULL){
+    while (head != NULL) {
         count++;
         head = head->nextItem;
     }
     return count;
 }
 
-ListItem *LinkedListGetFirst(ListItem *list){
-    if (list == NULL){
+ListItem *LinkedListGetFirst(ListItem *list) {
+    if (list == NULL) {
         return NULL;
     }
-    while (list->previousItem != NULL){
+    while (list->previousItem != NULL) {
         list = list->previousItem;
     }
     return list;
 }
 
-ListItem *LinkedListGetLast(ListItem *list){
-    if (list == NULL){
+ListItem *LinkedListGetLast(ListItem *list) {
+    if (list == NULL) {
         return NULL;
     }
-    while (list->nextItem != NULL){
+    while (list->nextItem != NULL) {
         list = list ->nextItem;
     }
     return list;
 }
 
-int LinkedListSwapData(ListItem *firstItem, ListItem *secondItem){
-    if (firstItem != NULL || secondItem != NULL){
+int LinkedListSwapData(ListItem *firstItem, ListItem *secondItem) {
+    if (firstItem != NULL || secondItem != NULL) {
         char *swap = firstItem->data;
         firstItem->data = secondItem->data;
-        secondItem-> = swap;
+        secondItem->data = swap;
         return SUCCESS;
     }
     return STANDARD_ERROR;
 }
 
-int LinkedListPrint(ListItem *list){
+int LinkedListPrint(ListItem *list) {
     ListItem *head;
-    if (list == NULL){
+    if (list == NULL) {
         return STANDARD_ERROR;
     }
     printf("[ ");
-    for (head = LinkedListGetFirst(list); head != NULL; 
-            head = head->nextItem){
+    for (head = LinkedListGetFirst(list); head != NULL;
+            head = head->nextItem) {
         printf("%s ", (head->data));
     }
     printf("]");
--- Lab05/Lab5.X/LinkedListTest.c
+++ Lab05/Lab5.X/LinkedListTest.c
@@ -14,12 +14,12 @@ int main() {
     
     char *first = "2B";
     char *second = "CP30";
-    char *third = "R2D2"";
+    char *third = "R2D2";
     char *fourth = "8B";
     
     printf("Creating a new list.\n");
     struct ListItem *a = LinkedListNew(first);
-    if (a->previousItem == NULL && a->data == NULL){
+    if (a->previousItem == NULL && a->data == first){
         printf("Success.\n");
     }
     
@@ -70,25 +70,40 @@ int main() {
     
     printf("Testing for last item.\n");
     struct ListItem *end = LinkedListGetLast(c);
-    if (end == c){
+    if (end == d){
         printf("Success. 1/2\n");
     }
     else{
         printf("Failed.\n");
     }
     struct ListItem *end_2 = LinkedListGetLast(a);
-    if (end_2 == c){
-        printf("Success. 1/2\n");
+    if (end_2 == d){
+        printf("Success. 2/2\n");
     }
     else{
         printf("Failed.\n");
     }
     
-    printf("Testing for printed list.");
-    struct ListItem *print_0 = LinkedListPrint(a);
+    printf("Testing for printed list.\n");
     printf("Expected: [ %s, %s, %s, %s]\n", first, second, third, fourth);
     printf("Result: \n");
+    LinkedListPrint(a);
     
+    printf("\nTesting for swapped items. Swapping second and third items.\n");
+    LinkedListSwapData(b, c);
+    printf("Expected: [ %s, %s, %s, %s]\n", first, third, second, fourth);
+    printf("Result: \n");
+    LinkedListPrint(a);
     
+    printf("\nTesting for removed items. Removing last item.\n");
+    LinkedListRemove(d);
+    printf("Expected: [ %s, %s, %s]\n", first, third, second);
+    printf("Result: \n");
+    LinkedListPrint(a);
+    printf("\nRemoving item two in list.\n");
+    LinkedListRemove(c);
+    printf("Expected: [ %s, %s]\n", first, third);
+    printf("Result: \n");
+    LinkedListPrint(a);
 }
 
