05d36fb6f164b7c4ddeb6543e369105131ca1d3f
Nadia Shabbar <nshabbar@ucsc.edu>
2022-05-03 14:17:36 -0700

    Progress on buttons.

--- Lab06/Lab6.X/Buttons.c
+++ Lab06/Lab6.X/Buttons.c
@@ -14,8 +14,10 @@
 //Variables in use
 static uint8_t debounce;
 uint8_t temp_status;
-uint8_t prev;
-uint8_t curr;
+static int prev_button1 = BUTTON_EVENT_1UP;
+static int prev_button2 = BUTTON_EVENT_2UP;
+static int prev_button3 = BUTTON_EVENT_3UP;
+static int prev_button4 = BUTTON_EVENT_4UP;
 
 struct Buttons {
     uint8_t Butt1;
@@ -33,31 +35,58 @@ uint8_t ButtonsCheckEvents(void) {
     uint8_t buttonsState = BUTTON_STATES();
     static uint8_t EVENT = 0;
     if (debounce == BUTTONS_DEBOUNCE_PERIOD) {
-        if (buttonsState & BUTTON_STATE_1 ) {
+        if ((prev_button1 = BUTTON_EVENT_1UP) && 
+                (buttonsState & BUTTON_STATE_1)) {
             status.Butt1 = BUTTON_EVENT_1DOWN;
+            prev_button1 = BUTTON_EVENT_1DOWN;
         }
-        if (buttonsState & BUTTON_STATE_2) {
+        if ((prev_button2 = BUTTON_EVENT_2UP) && 
+                (buttonsState & BUTTON_STATE_2)) {
             status.Butt2 = BUTTON_EVENT_2DOWN;
-        } 
-        if (buttonsState & BUTTON_STATE_3) {
-            status.Butt3 = BUTTON_EVENT_3DOWN;
+            prev_button2 = BUTTON_EVENT_2DOWN;
         }
-        if (buttonsState & BUTTON_STATE_4) {
+        if ((prev_button3 = BUTTON_EVENT_3UP) && 
+                (buttonsState & BUTTON_STATE_3)) {
+            status.Butt3 = BUTTON_EVENT_3DOWN;
+            prev_button3 = BUTTON_EVENT_3DOWN;
+        }
+        if ((prev_button4 = BUTTON_EVENT_4UP) && 
+                (buttonsState & BUTTON_STATE_4)) {
             status.Butt4 = BUTTON_EVENT_4DOWN;
+            prev_button4 = BUTTON_EVENT_4DOWN;
+        }
+        if ((prev_button1 = BUTTON_EVENT_1DOWN) && 
+                (buttonsState & BUTTON_STATE_1) == 0) {
+            status.Butt1 = BUTTON_EVENT_1UP;
+            prev_button1 = BUTTON_EVENT_1UP;
+        }
+        if ((prev_button2 = BUTTON_EVENT_2DOWN) && 
+                (buttonsState & BUTTON_STATE_2) == 0) {
+            status.Butt2 = BUTTON_EVENT_2UP;
+            prev_button2 = BUTTON_EVENT_2UP;
+        }
+        if ((prev_button3 = BUTTON_EVENT_3DOWN) && 
+                (buttonsState & BUTTON_STATE_3) == 0) {
+            status.Butt3 = BUTTON_EVENT_3UP;
+            prev_button3 = BUTTON_EVENT_3UP;
+        }
+        if ((prev_button4 = BUTTON_EVENT_4DOWN) && 
+                (buttonsState & BUTTON_STATE_4) == 0) {
+            status.Butt4 = BUTTON_EVENT_4UP;
+            prev_button4 = BUTTON_EVENT_4UP;
         }
         temp_status = status.Butt1 | status.Butt2;
         temp_status |= status.Butt3;
         temp_status |= status.Butt4;
-        if (temp_status == EVENT){
+        if (temp_status == EVENT) {
             return BUTTON_EVENT_NONE;
-        }
-        else{
+        } else {
+            EVENT = temp_status;
             debounce = 0;
+            return EVENT;
         }
-    }
-    else{
+    } else {
         debounce++;
+        return BUTTON_EVENT_NONE;
     }
-
 }
-
--- Lab06/Lab6.X/ButtonsTest.c
+++ Lab06/Lab6.X/ButtonsTest.c
@@ -19,8 +19,8 @@
 // **** Declare any datatypes here ****
 
 // **** Define global, module-level, or external variables here ****
-uint8_t Button;
-
+static uint8_t Button;
+static uint8_t prog = FALSE;
 // **** Declare function prototypes ****
 
 int main(void) {
@@ -43,33 +43,53 @@ int main(void) {
      * Your code goes in between this comment and the following one with asterisks.
      **************************************************************************************************/
     printf("Welcome to nshabbar's lab6 part4 (ButtonsTest).  Compiled on %s %s.\n", __TIME__, __DATE__);
-    
+
 
     printf("Please press some buttons!\n");
     uint8_t status = BUTTON_EVENT_NONE;
     ButtonsInit();
-    char Butt1[20] = "----";
-    char Butt2[20] = "----";
-    char Butt3[20] = "----";
-    char Butt4[20] = "----";
 
     while (1) {
-        if (Button == status) {
-            printf("Event: 4:%s 3:%s 2:%s 1:%s\n", Butt4, Butt3, Butt2, Butt1);
-        } else if (Button & BUTTON_EVENT_1DOWN) {
-            strcpy(Butt1,"DOWN");
-            printf("Event: 4:%s 3:%s 2:%s 1:%s\n", Butt4, Butt3, Butt2, Butt1);
-        } else if (Button & BUTTON_EVENT_2DOWN) {
-            strcpy(Butt2,"DOWN");
-            printf("Event: 4:%s 3:%s 2:%s 1:%s\n", Butt4, Butt3, Butt2, Butt1);
-        } else if (Button & BUTTON_EVENT_3DOWN) {
-            strcpy(Butt3,"DOWN");
-            printf("Event: 4:%s 3:%s 2:%s 1:%s\n", Butt4, Butt3, Butt2, Butt1);
-        } else if (Button & BUTTON_EVENT_4DOWN) {
-            strcpy(Butt4,"DOWN");
-            printf("Event: 4:%s 3:%s 2:%s 1:%s\n", Butt4, Butt3, Butt2, Butt1);
+        char Butt1[20] = "----";
+        char Butt2[20] = "----";
+        char Butt3[20] = "----";
+        char Butt4[20] = "----";
+
+        if (prog == TRUE) {
+            if (Button == status) {
+                printf("Event: 4:%s 3:%s 2:%s 1:%s\n", Butt4, Butt3, Butt2, Butt1);
+            }
+            if (Button & BUTTON_EVENT_1DOWN) {
+                strcpy(Butt1, "DOWN");
+                printf("Event: 4:%s 3:%s 2:%s 1:%s\n", Butt4, Butt3, Butt2, Butt1);
+            } else if (Button & BUTTON_EVENT_1UP) {
+                strcpy(Butt1, "UP");
+                printf("Event: 4:%s 3:%s 2:%s 1:%s\n", Butt4, Butt3, Butt2, Butt1);
+            }
+            if (Button & BUTTON_EVENT_2DOWN) {
+                strcpy(Butt2, "DOWN");
+                printf("Event: 4:%s 3:%s 2:%s 1:%s\n", Butt4, Butt3, Butt2, Butt1);
+            } else if (Button & BUTTON_EVENT_2UP) {
+                strcpy(Butt2, "UP");
+                printf("Event: 4:%s 3:%s 2:%s 1:%s\n", Butt4, Butt3, Butt2, Butt1);
+            }
+            if (Button & BUTTON_EVENT_3DOWN) {
+                strcpy(Butt3, "DOWN");
+                printf("Event: 4:%s 3:%s 2:%s 1:%s\n", Butt4, Butt3, Butt2, Butt1);
+            } else if (Button & BUTTON_EVENT_3UP) {
+                strcpy(Butt3, "UP");
+                printf("Event: 4:%s 3:%s 2:%s 1:%s\n", Butt4, Butt3, Butt2, Butt1);
+            }
+            if (Button & BUTTON_EVENT_4DOWN) {
+                strcpy(Butt4, "DOWN");
+                printf("Event: 4:%s 3:%s 2:%s 1:%s\n", Butt4, Butt3, Butt2, Butt1);
+            } else if (Button & BUTTON_EVENT_4UP) {
+                strcpy(Butt4, "UP");
+                printf("Event: 4:%s 3:%s 2:%s 1:%s\n", Butt4, Butt3, Butt2, Butt1);
+            }
         }
     }
+
 
     /***************************************************************************************************
      * Your code goes in between this comment and the preceding one with asterisks
@@ -92,7 +112,9 @@ void __ISR(_TIMER_1_VECTOR, ipl4auto) Timer1Handler(void) {
      * Your code goes in between this comment and the following one with asterisks.
      **************************************************************************************************/
     Button = ButtonsCheckEvents();
-
+    if (Button != BUTTON_EVENT_NONE) {
+        prog = TRUE;
+    }
     /***************************************************************************************************
      * Your code goes in between this comment and the preceding one with asterisks
      **************************************************************************************************/
