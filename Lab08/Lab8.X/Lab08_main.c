b55937ad432d43bbf1a8372f5b2f3775b4fb1893
Nadia Shabbar <nshabbar@ucsc.edu>
2022-05-16 22:12:18 -0700

    Ok, I think everything is defined and I can test now.

--- Lab08/Lab8.X/Lab08_main.c
+++ Lab08/Lab8.X/Lab08_main.c
@@ -22,7 +22,7 @@
 // **** Declare any data types here ****
 
 // **** Define any global or external variables here ****
-static uint8_t exists;
+static uint8_t exit;
 static char input;
 static char Title[GAME_MAX_ROOM_TITLE_LENGTH +1];
 static char Description[GAME_MAX_ROOM_DESC_LENGTH + 1];
@@ -45,6 +45,58 @@ int main()
         //Display Title
         GameGetCurrentRoomTitle(Title);
         printf("\nROOM TITLE: %s\n", Title);
+        
+        GameGetCurrentRoomDescription(Description);
+        printf("\nROOM DESCRIPTION: %s\n", Description);
+        
+        exit = GameGetCurrentRoomExits();
+        printf("You may leave in the following direction(s): \n");
+        if( exit & GAME_ROOM_EXIT_NORTH_EXISTS){
+            printf("NORTH. \n");
+        }
+        if( exit & GAME_ROOM_EXIT_SOUTH_EXISTS){
+            printf("SOUTH. \n");
+        }
+        if( exit & GAME_ROOM_EXIT_WEST_EXISTS){
+            printf("WEST. \n");
+        }
+        if( exit & GAME_ROOM_EXIT_EAST_EXISTS){
+            printf("EAST. \n");
+        }
+        printf("\nPress one of the following keys: [n, s, w, e, q]\n "
+                "(North, South, West, East, and Quit\n");
+        input = getchar();
+        printf("User's input: %c\n", input);
+        if (getchar() != "\n"){
+            continue;
+        }
+        if(input == "n" || input == "N"){
+            if (!GameGoNorth()){
+                printf("Invalid Input. No exit or entrance in that direction.");
+            }
+        }
+        if(input == "s" || input == "S"){
+            if (!GameGoSouth()){
+                printf("Invalid Input. No exit or entrance in that direction.");
+            }
+        }
+        if(input == "w" || input == "W"){
+            if (!GameGoWest()){
+                printf("Invalid Input. No exit or entrance in that direction.");
+            }
+        }
+        if(input == "e" || input == "E"){
+            if (!GameGoEast()){
+                printf("Invalid Input. No exit or entrance in that direction.");
+            }
+        }
+        if (input == "q" || input == "Q"){
+            printf("You've lost the will to continue. "
+                    "The world goes black around you.");
+        }
+        else{
+            printf("Invalid entry. Please select a correct entry.");
+        }
     }
 
 
