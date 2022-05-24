84de7407446f2cff6b100918ab0bd9db8dda4510
Nadia Shabbar <nshabbar@ucsc.edu>
2022-05-23 21:44:47 -0700

    Finished up message and negotiations.

--- Lab09/Lab9.X/Negotiation.c
+++ Lab09/Lab9.X/Negotiation.c
@@ -1,18 +1,37 @@
-/* 
- * File:   Negotiation.c
- * Author: Nadia
- *
- * Created on May 23, 2022, 6:06 PM
- */
-
 #include <stdio.h>
 #include <stdlib.h>
+#include "Negotiation.h"
+#include "BOARD.h"
 
-/*
- * 
- */
-int main(int argc, char** argv) {
+#define SIXTEEN 16
+#define TWO 2
+#define HEX 0x01
 
-    return (EXIT_SUCCESS);
+static NegotiationData result;
+
+NegotiationData NegotiationHash(NegotiationData secret) {
+    result = (secret * secret) % PUBLIC_KEY;
+    return result;
 }
 
+int NegotiationVerify(NegotiationData secret, NegotiationData commitment) {
+    if (NegotiationHash(secret) == commitment) {
+        return TRUE;
+    } else {
+        return FALSE;
+    }
+}
+
+NegotiationOutcome NegotiateCoinFlip(NegotiationData A, NegotiationData B) {
+    NegotiationData Result = A ^ B;
+    uint8_t i = 0, count = 0;
+
+    for (i = 0; i < SIXTEEN; i++) {
+        count += ((Result >> i) & HEX);
+    }
+    if (count % TWO) {
+        return TAILS;
+    } else {
+        return HEADS;
+    }
+}
